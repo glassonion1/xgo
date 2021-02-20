261830dd50f9d58b592e510a6ae1bd0f02e0f21e
taisuke.fujita <glassonion999@gmail.com>
2021-02-20 17:19:41 +0900

    Modify the retry function

--- retry.go
+++ retry.go
@@ -6,11 +6,6 @@ import (
 	"time"
 )
 
-// Retrier decides whether to retry a failed function
-type Retrier interface {
-	Perform(func() error, func(err error) bool) error
-}
-
 // ExponentialBackoff represents retry with exponential backoff
 type ExponentialBackoff struct {
 	maxRetries      int
@@ -26,22 +21,22 @@ func NewExponentialBackoff() *ExponentialBackoff {
 }
 
 // Perform the exponential backoff algorithm
-func (retrier *ExponentialBackoff) Perform(fn func() error, retryCondition func(err error) bool) error {
+func (eb *ExponentialBackoff) Perform(fn func() error, retryCondition func(err error) bool) error {
 	population := int64(time.Second / time.Millisecond)
 
 	var err error
 	var backoff time.Duration
 	var ms time.Duration
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < retrier.maxRetries; i++ {
+	for i := 0; i < eb.maxRetries; i++ {
 		err = fn()
 		if !retryCondition(err) {
 			return nil
 		}
 		ms = time.Duration(r.Int63n(population)) * time.Millisecond
 		backoff = time.Duration(math.Exp2(float64(i)))*time.Second + ms
-		if backoff > retrier.maxRetrySeconds {
-			backoff = retrier.maxRetrySeconds
+		if backoff > eb.maxRetrySeconds {
+			backoff = eb.maxRetrySeconds
 		}
 		time.Sleep(backoff)
 	}

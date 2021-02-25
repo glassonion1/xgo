1c8993350c742a3c59bc99482f560cb0e4832527
taisuke.fujita <glassonion999@gmail.com>
2021-02-25 09:42:40 +0900

    Add a sample code

--- example_test.go
+++ example_test.go
@@ -34,7 +34,53 @@ func ExampleDeepCopy() {
 	if err != nil {
 		// handles error
 	}
-	fmt.Printf("prints ToModel object: %v", to)
+	fmt.Println("ToModel object:", to)
 
-	// Output: prints ToModel object: &{xxxx foo seconds:1590969600 seconds:1590969600}
+	// Output: ToModel object: &{xxxx foo seconds:1590969600 seconds:1590969600}
+}
+
+func ExampleContains() {
+	// slice of int32
+	containsInt32 := xgo.Contains([]int32{1, 2, 3, 4, 5}, 3)
+	fmt.Println("contains int32:", containsInt32)
+
+	// slice of int
+	containsInt := xgo.Contains([]int{1, 2, 3, 4, 5}, 2)
+	fmt.Println("contains int:", containsInt)
+
+	// slice of float64
+	containsFloat64 := xgo.Contains([]float64{1.1, 2.2, 3.3, 4.4, 5.5}, 4.4)
+	fmt.Println("contains float64:", containsFloat64)
+
+	// slice of struct
+	type item struct {
+		ID   string
+		Name string
+	}
+	list := []item{
+		item{
+			ID:   "1",
+			Name: "test1",
+		},
+		item{
+			ID:   "2",
+			Name: "test2",
+		},
+		item{
+			ID:   "3",
+			Name: "test3",
+		},
+	}
+	target := item{
+		ID:   "2",
+		Name: "test2",
+	}
+	containsStruct := xgo.Contains(list, target)
+	fmt.Println("contains struct:", containsStruct)
+
+	// Output:
+	// contains int32: true
+	// contains int: true
+	// contains float64: true
+	// contains struct: true
 }

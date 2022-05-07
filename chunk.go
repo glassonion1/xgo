dd2cb4b29b6c8ae256f17b27a02287805ffb5d41
glassonion1 <taisuke.fujita@layerx.co.jp>
2022-05-08 00:52:27 +0900

    Add chunk function

--- chunk.go
+++ chunk.go
@@ -1,23 +1,23 @@
 package xgo
 
-// Chunk is chunked index that contains from and to
-type Chunk struct {
+// Index is chunked index that contains from and to
+type ChunkedIndex struct {
 	From, To int
 }
 
-// SplitChunks splits chunks from slice length
-func SplitChunks(length int, chunkSize int) <-chan Chunk {
+// ChunkedIndex divides index into chunks
+func ChunkIndex(length int, chunkSize int) <-chan ChunkedIndex {
 	if chunkSize == 0 {
 		chunkSize = 1
 	}
 
-	ch := make(chan Chunk)
+	ch := make(chan ChunkedIndex)
 
 	go func() {
 		defer close(ch)
 
 		for i := 0; i < length; i += chunkSize {
-			idx := Chunk{i, i + chunkSize}
+			idx := ChunkedIndex{i, i + chunkSize}
 			if length < idx.To {
 				idx.To = length
 			}
@@ -26,4 +26,15 @@ func SplitChunks(length int, chunkSize int) <-chan Chunk {
 	}()
 
 	return ch
+}
+
+func Chunk[T any](list []T, chunkSize int) [][]T {
+	var chunks [][]T
+
+	for chunkSize < len(list) {
+		list, chunks = list[chunkSize:], append(chunks, list[0:chunkSize:chunkSize])
+	}
+	chunks = append(chunks, list)
+
+	return chunks
 }
--- chunk_test.go
+++ chunk_test.go
@@ -3,12 +3,11 @@ package xgo_test
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-
 	"github.com/glassonion1/xgo"
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestSplitChunk(t *testing.T) {
+func TestChunkIndex(t *testing.T) {
 
 	type args struct {
 		length    int
@@ -18,7 +17,7 @@ func TestSplitChunk(t *testing.T) {
 	tests := []struct {
 		name string
 		in   args
-		want []xgo.Chunk
+		want []xgo.ChunkedIndex
 	}{
 		{
 			name: "splits chunks by evne",
@@ -26,7 +25,7 @@ func TestSplitChunk(t *testing.T) {
 				length:    6,
 				chunkSize: 2,
 			},
-			want: []xgo.Chunk{
+			want: []xgo.ChunkedIndex{
 				{
 					From: 0,
 					To:   2,
@@ -47,7 +46,7 @@ func TestSplitChunk(t *testing.T) {
 				length:    5,
 				chunkSize: 2,
 			},
-			want: []xgo.Chunk{
+			want: []xgo.ChunkedIndex{
 				{
 					From: 0,
 					To:   2,
@@ -68,7 +67,7 @@ func TestSplitChunk(t *testing.T) {
 				length:    10,
 				chunkSize: 3,
 			},
-			want: []xgo.Chunk{
+			want: []xgo.ChunkedIndex{
 				{
 					From: 0,
 					To:   3,
@@ -93,7 +92,7 @@ func TestSplitChunk(t *testing.T) {
 				length:    5,
 				chunkSize: 10,
 			},
-			want: []xgo.Chunk{
+			want: []xgo.ChunkedIndex{
 				{
 					From: 0,
 					To:   5,
@@ -106,7 +105,7 @@ func TestSplitChunk(t *testing.T) {
 				length:    3,
 				chunkSize: 0,
 			},
-			want: []xgo.Chunk{
+			want: []xgo.ChunkedIndex{
 				{
 					From: 0,
 					To:   1,
@@ -127,8 +126,8 @@ func TestSplitChunk(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			cgot := xgo.SplitChunks(tt.in.length, tt.in.chunkSize)
-			var got []xgo.Chunk
+			cgot := xgo.ChunkIndex(tt.in.length, tt.in.chunkSize)
+			var got []xgo.ChunkedIndex
 			for i := range cgot {
 				got = append(got, i)
 			}
@@ -147,12 +146,165 @@ func TestSplitChunk(t *testing.T) {
 			{14, 16},
 		}
 		index := 0
-		for chunk := range xgo.SplitChunks(len(list), 3) {
+		for chunk := range xgo.ChunkIndex(len(list), 3) {
 			got := list[chunk.From:chunk.To]
 			if diff := cmp.Diff(wants[index], got); diff != "" {
 				t.Errorf("testing %s mismatch (-want +got):\n%s\n", "tests slice", diff)
 			}
 			index++
+		}
+	})
+}
+
+func TestChunk(t *testing.T) {
+	t.Run("tests slice of int", func(t *testing.T) {
+		list := []int{2, 4, 6, 8, 10, 12, 14, 16}
+		want := [][]int{
+			{2, 4, 6},
+			{8, 10, 12},
+			{14, 16},
+		}
+		got := xgo.Chunk[int](list, 3)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("testing %s mismatch (-want +got):\n%s\n", "tests slice", diff)
+		}
+	})
+
+	t.Run("tests slice of struct", func(t *testing.T) {
+		type item struct {
+			ID   string
+			Name string
+		}
+		list := []item{
+			{
+				ID:   "1",
+				Name: "test1",
+			},
+			{
+				ID:   "2",
+				Name: "test2",
+			},
+			{
+				ID:   "3",
+				Name: "test3",
+			},
+			{
+				ID:   "4",
+				Name: "test4",
+			},
+			{
+				ID:   "5",
+				Name: "test5",
+			},
+			{
+				ID:   "6",
+				Name: "test6",
+			},
+		}
+		want := [][]item{
+			{
+				{
+					ID:   "1",
+					Name: "test1",
+				},
+				{
+					ID:   "2",
+					Name: "test2",
+				},
+			},
+			{
+				{
+					ID:   "3",
+					Name: "test3",
+				},
+				{
+					ID:   "4",
+					Name: "test4",
+				},
+			},
+			{
+				{
+					ID:   "5",
+					Name: "test5",
+				},
+				{
+					ID:   "6",
+					Name: "test6",
+				},
+			},
+		}
+		got := xgo.Chunk[item](list, 2)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("testing %s mismatch (-want +got):\n%s\n", "tests slice", diff)
+		}
+	})
+
+	t.Run("tests slice of pointer", func(t *testing.T) {
+		type item struct {
+			ID   string
+			Name string
+		}
+		list := []*item{
+			{
+				ID:   "1",
+				Name: "test1",
+			},
+			{
+				ID:   "2",
+				Name: "test2",
+			},
+			{
+				ID:   "3",
+				Name: "test3",
+			},
+			{
+				ID:   "4",
+				Name: "test4",
+			},
+			{
+				ID:   "5",
+				Name: "test5",
+			},
+			{
+				ID:   "6",
+				Name: "test6",
+			},
+		}
+		want := [][]*item{
+			{
+				{
+					ID:   "1",
+					Name: "test1",
+				},
+				{
+					ID:   "2",
+					Name: "test2",
+				},
+			},
+			{
+				{
+					ID:   "3",
+					Name: "test3",
+				},
+				{
+					ID:   "4",
+					Name: "test4",
+				},
+			},
+			{
+				{
+					ID:   "5",
+					Name: "test5",
+				},
+				{
+					ID:   "6",
+					Name: "test6",
+				},
+			},
+		}
+		got := xgo.Chunk[*item](list, 2)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("testing %s mismatch (-want +got):\n%s\n", "tests slice", diff)
 		}
 	})
 }

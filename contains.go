ff2866354efbc5e1e341ef722a4b979f97c52479
glassonion1 <taisuke.fujita@layerx.co.jp>
2022-05-08 01:04:28 +0900

    fix for lint

--- contains.go
+++ contains.go
@@ -4,31 +4,10 @@ import "reflect"
 
 // Contains returns true if an element is present in a slice
 func Contains[T any](list []T, elem T) bool {
-	defer func() {
-		recover()
-	}()
-
 	for _, item := range list {
 		if ok := reflect.DeepEqual(item, elem); ok {
 			return true
 		}
 	}
 	return false
-
-	/*
-		listV := reflect.ValueOf(list)
-
-		if listV.Kind() == reflect.Slice {
-			for i := 0; i < listV.Len(); i++ {
-				item := listV.Index(i).Interface()
-
-				target := reflect.ValueOf(elem).Convert(reflect.TypeOf(item)).Interface()
-
-				if ok := reflect.DeepEqual(item, target); ok {
-					return true
-				}
-			}
-		}
-		return false
-	*/
 }
